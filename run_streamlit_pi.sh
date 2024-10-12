c20d5930dfc551e3ff475171b7437d5099bde27f
phawitb <96680592+phawitb@users.noreply.github.com>
2024-10-12 14:40:53 +0700

    Update run_streamlit_pi.sh

--- run_streamlit_pi.sh
+++ run_streamlit_pi.sh
@@ -4,11 +4,19 @@ sleep 5
 
 #source /home/phawit/anaconda3/etc/profile.d/conda.sh  # Adjust this path if necessary
 #conda activate rifle  # Replace 'your_env_name' with your actual environment name
+
 cd /home/phawit/Documents
-rm -r ArmyStock2
-sleep 1
-git clone git clone https://github.com/phawitb/ArmyStock2.git
-sleep 3
+
+URL="8.8.8.8"  # Google Public DNS
+if ping -c 1 "$URL" &> /dev/null; then
+    echo "online"
+    rm -r ArmyStock2
+    sleep 1
+    git clone https://github.com/phawitb/ArmyStock2.git
+    sleep 3
+fi
+
+cd /home/phawit/Documents/ArmyStock2
 python3 load_data_from_googlesheet.py
 
 sleep 3
@@ -18,4 +26,3 @@ python3 -m streamlit run app.py --server.headless true --server.port 8509 &
 sleep 5
 
 chromium-browser --start-fullscreen http://localhost:8509
-
